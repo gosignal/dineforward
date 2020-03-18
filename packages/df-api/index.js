025c759b3b1aa28ed8a9a592380cdef3aa358bea
Mark Terrel <mark@unbounded.systems>
2020-03-18 16:57:16 -0600

    Enable Redis for API session store

--- packages/df-api/index.js
+++ packages/df-api/index.js
@@ -12,11 +12,39 @@ const { useAuthStrategy } = require('./utils/accessControl');
 
 const PROJECT_NAME = 'DineForward - Backend';
 
-const keystone = new Keystone({
+const keystoneConfig = {
   name: PROJECT_NAME,
   adapter: new Adapter(),
   onConnect: initialiseData,
-});
+};
+
+function initRedis(kConfig) {
+  if (!process.env.REDIS_HOST || process.env.KEYSTONE_BUILD != null) return;
+
+  const redis = require('redis');
+  const session = require('express-session');
+  const RedisStore = require('connect-redis')(session);
+
+  const clientConfig = {
+    host: process.env.REDIS_HOST,
+  };
+
+  if (process.env.REDIS_DBNAME) clientConfig.db = process.env.REDIS_DBNAME;
+  if (process.env.REDIS_PORT) {
+    const portNum = parseInt(process.env.REDIS_PORT, 10);
+    if (isNaN(portNum)) throw new Error(`REDIS_PORT must be an integer`);
+    clientConfig.port = portNum;
+  }
+  if (process.env.REDIS_PASSWORD) clientConfig.password = process.env.REDIS_PASSWORD;
+
+  const client = redis.createClient(clientConfig);
+
+  kConfig.sessionStore = new RedisStore({ client });
+}
+
+initRedis(keystoneConfig);
+
+const keystone = new Keystone(keystoneConfig);
 
 // instantiate our middleware
 registerAppLists(keystone);
