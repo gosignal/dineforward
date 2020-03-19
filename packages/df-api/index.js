54e6865f31adc2cce416eee9a2f5e712fc49fd6e
William Kasel <wkasel@gmail.com>
2020-03-19 13:48:03 -0700

    Update

--- packages/df-api/index.js
+++ packages/df-api/index.js
@@ -8,6 +8,7 @@ const { NextApp } = require('@keystonejs/app-next');
 const initialiseData = require('./initial-data');
 const { registerAppLists } = require('./schema');
 const { useAuthStrategy } = require('./utils/accessControl');
+const { initRedis } = require('./initRedis');
 
 const PROJECT_NAME = 'DineForward - Backend';
 
@@ -16,31 +17,6 @@ const keystoneConfig = {
   adapter: new MongooseAdapter(),
   onConnect: initialiseData,
 };
-
-function initRedis(kConfig) {
-  if (!process.env.REDIS_HOST || process.env.KEYSTONE_BUILD != null) return;
-
-  const redis = require('redis');
-  const session = require('express-session');
-
-  const RedisStore = require('connect-redis')(session);
-
-  const clientConfig = {
-    host: process.env.REDIS_HOST,
-  };
-
-  if (process.env.REDIS_DBNAME) clientConfig.db = process.env.REDIS_DBNAME;
-  if (process.env.REDIS_PORT) {
-    const portNum = parseInt(process.env.REDIS_PORT, 10);
-    if (isNaN(portNum)) throw new Error(`REDIS_PORT must be an integer`);
-    clientConfig.port = portNum;
-  }
-  if (process.env.REDIS_PASSWORD) clientConfig.password = process.env.REDIS_PASSWORD;
-
-  const client = redis.createClient(clientConfig);
-
-  kConfig.sessionStore = new RedisStore({ client });
-}
 
 initRedis(keystoneConfig);
 
---
+++ packages/df-api/initRedis.js
@@ -0,0 +1,28 @@
+function initRedis(kConfig) {
+  if (!process.env.REDIS_HOST || process.env.KEYSTONE_BUILD != null) return;
+
+  const redis = require('redis');
+  const session = require('express-session');
+
+  const RedisStore = require('connect-redis')(session);
+
+  const clientConfig = {
+    host: process.env.REDIS_HOST,
+  };
+
+  if (process.env.REDIS_DBNAME) clientConfig.db = process.env.REDIS_DBNAME;
+  if (process.env.REDIS_PORT) {
+    const portNum = parseInt(process.env.REDIS_PORT, 10);
+    if (isNaN(portNum)) throw new Error(`REDIS_PORT must be an integer`);
+    clientConfig.port = portNum;
+  }
+  if (process.env.REDIS_PASSWORD) clientConfig.password = process.env.REDIS_PASSWORD;
+
+  const client = redis.createClient(clientConfig);
+
+  kConfig.sessionStore = new RedisStore({ client });
+}
+
+module.exports = {
+  initRedis,
+};
