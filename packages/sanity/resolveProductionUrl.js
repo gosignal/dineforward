2628ce32d877d5b7d10a5a0acafbf0ffd995d259
William Kasel <wkasel@gmail.com>
2020-03-21 23:59:32 -0700

    Removing Sanity... literally... GO INSANE

--- packages/sanity/resolveProductionUrl.js
+++
@@ -1,6 +0,0 @@
-const previewSecret = 'asd9asdasidasudasud9465-a';
-const projectUrl = 'http://localhost:3000';
-
-export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
-}
--- packages/sanity/schemas/locale/supportedLanguages.js
+++
@@ -1,1 +0,0 @@
-export default [{ id: 'en', title: 'English', isDefault: true }, { id: 'es', title: 'Spanish' }];
