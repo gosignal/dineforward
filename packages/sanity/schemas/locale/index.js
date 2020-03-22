b5db91cf3f8efcfc5ebfefd1743139396a3feab3
William Kasel <wkasel@gmail.com>
2020-03-21 23:59:32 -0700

    Removing Sanity... literally... GO INSANE

--- packages/sanity/schemas/locale/index.js
+++
@@ -1,8 +0,0 @@
-import localeBlockContent from './BlockContent';
-import localeString from './String';
-import localeText from './Text';
-import SupportedLanguages from './supportedLanguages';
-
-export { SupportedLanguages };
-
-export default { localeBlockContent, localeString, localeText };
