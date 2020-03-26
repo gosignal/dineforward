c5a03aa3d21038a76ca07dd74d2d19ab23b4c6c2
William Kasel <wkasel@gmail.com>
2020-03-25 21:20:37 -0700

    Updated manage

--- packages/df-client/src/pages/account/manage.jsx
+++ packages/df-client/src/pages/account/manage.jsx
@@ -1,15 +1,5 @@
 import React, { Fragment } from 'react';
-// import { request } from 'graphql-request';
-
-import { withStyles, Theme, Container, Grid, Slide, Typography, AppBar } from '@material-ui/core';
 import { withIdentity } from '~utils/withIdentity';
-
-// import NewLayout from '~components/NewLayout';
-
-const API_URL = process.env.API_URL || 'http://localhost:8000/admin/api';
-
-// import { checkLoggedIn } from '~gql/checkLoggedIn';
-
 import Admin from '~containers/Admin';
 
 const AccountManagePage = props => {
