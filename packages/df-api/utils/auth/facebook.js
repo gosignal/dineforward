b54e1b3a56896c92bddd0848dd268922a3190b02
Mark Terrel <mark@unbounded.systems>
2020-03-27 14:03:46 -0600

    Ensure Facebook auth always returns an email

--- packages/df-api/utils/auth/facebook.js
+++ packages/df-api/utils/auth/facebook.js
@@ -3,16 +3,21 @@ const { FacebookAuthStrategy } = require('@keystonejs/auth-passport');
 
 const init = (ks, commonCfg) => {
   if (!FACEBOOK_APP_ID || !FACEBOOK_APP_SECRET) return;
+  const config = {
+    ...commonCfg,
+    appId: FACEBOOK_APP_ID,
+    appSecret: FACEBOOK_APP_SECRET,
+    scope: [ 'public_profile', 'email' ],
+    idField: 'facebookId',
+  };
+  config.strategyConfig = {
+    ...config.strategyConfig,
+    profileFields: ['id', 'displayName', 'photos', 'email'],
+  }
   return ks.createAuthStrategy({
     type: FacebookAuthStrategy,
     list: 'User',
-    config: {
-      ...commonCfg,
-      appId: FACEBOOK_APP_ID,
-      appSecret: FACEBOOK_APP_SECRET,
-      scope: [ 'public_profile', 'email' ],
-      idField: 'facebookId',
-    },
+    config,
   });
 }
 
