454c74560666449f6ea9c11b51313d6ca94c08bf
William Kasel <wkasel@gmail.com>
2020-03-31 04:02:18 -0700

    Added Link home in nav

--- packages/df-client/src/views/components/DynamicContent/index.jsx
+++ packages/df-client/src/views/components/DynamicContent/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ContentPageLayout from '~containers/Layouts/contentpage.layout';
+import { makeStyles } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  root: {},
+  dynamicContent: {
+    marginTop: '200px',
+  },
+});
 const DynamicContent = ({ name, blocks, subtitle }) => {
+  const classes = useStyles();
   return (
     <ContentPageLayout>
-      <h1>{name}</h1>
-      <b>{subtitle}</b>
-      {blocks.map((b, id) => (
-        <div key={id} dangerouslySetInnerHTML={{ __html: b.richContent }} />
-      ))}
+      <div className={classes.dynamicContent}>
+        <h1>{name}</h1>
+        <b>{subtitle}</b>
+        {blocks.map((b, id) => (
+          <div key={id} dangerouslySetInnerHTML={{ __html: b.richContent }} />
+        ))}
+      </div>
     </ContentPageLayout>
   );
 };
