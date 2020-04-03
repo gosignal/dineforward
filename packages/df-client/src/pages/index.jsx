aee256c6434bf73982b62bbfd19d58195c351449
Mark Terrel <mark@unbounded.systems>
2020-04-02 23:22:45 -0600

    home: style large restaurant button

--- packages/df-client/src/pages/index.jsx
+++ packages/df-client/src/pages/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {
-  Grid,
-  Container,
-} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/styles';
+import Link from 'next/link';
 import { withApollo } from '~utils/apollo';
 // import ParticipatingRestaurants from '~components/ParticipatingRestaurants';
-import Subscribe from '~components/Subscribe';
+// import Subscribe from '~components/Subscribe';
 import AppNav from '~components/AppNav';
 import { HowItWorks, HeroBannerSection } from '~containers/Sections';
 import HomePageLayout from '~containers/Layouts/homepage.layout';
@@ -15,11 +14,35 @@ const useStyles = makeStyles({
   root: {
     padding: 0,
   },
+  buttonBox: {
+    marginBottom: '50px',
+    marginTop: '50px',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+  restaurantButton: {
+    paddingBottom: '1.2em',
+    paddingTop: '1.2em',
+  },
 });
 
-const Image = () => {
-  return <img src="" />;
-};
+const RestaurantButton = ({ classes }) => (
+  <Grid container className={classes.buttonBox} justify="center">
+    <Grid item sm={8} xs={12}>
+      <Link href="/restaurants">
+        <Button
+          color="primary"
+          fullWidth
+          classes={{ root: classes.restaurantButton }}
+          variant="contained"
+          size="large"
+        >
+          Are you a restaurant?
+        </Button>
+      </Link>
+    </Grid>
+  </Grid>
+);
 
 const HomePage = props => {
   const classes = useStyles();
@@ -27,12 +50,8 @@ const HomePage = props => {
     <HomePageLayout>
       <AppNav />
       <HeroBannerSection />
-      <Grid container>
-        <HowItWorks />
-      </Grid>
-      <Container className={classes.root} maxWidth="lg">
-        <Subscribe />
-      </Container>
+      <HowItWorks />
+      <RestaurantButton classes={classes} />
     </HomePageLayout>
   );
 };
