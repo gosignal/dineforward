0bca32943f341dc5b314673dac7ec7e3354a2f29
Mark Terrel <mark@unbounded.systems>
2020-04-03 03:30:33 -0600

    Updated side nav to match requested links

--- packages/df-client/src/views/components/AppNav/NavRoutes.jsx
+++ packages/df-client/src/views/components/AppNav/NavRoutes.jsx
@@ -1,41 +1,36 @@
-import {
-  Star as StarIcon,
-  Business as BusinessIcon,
-  LocalOffer as LocalOfferIcon,
-  Settings as SettingsIcon,
-  CreditCard as CreditCardIcon,
-  Info as InfoIcon,
-  Help as HelpIcon,
-} from '@material-ui/icons';
+import Email from '@material-ui/icons/Email';
+import Home from '@material-ui/icons/Home';
+import Info from '@material-ui/icons/Info';
+import FAQ from '@material-ui/icons/QuestionAnswer';
+import Restaurant from '@material-ui/icons/Restaurant';
 
-// const baseRoute = 'http://localhost:8000';
 const baseRoute = '';
 
 export const navRoutes = [
   {
     link: `${baseRoute}/`,
     name: 'Home',
-    Icon: () => <StarIcon />,
+    Icon: Home,
   },
   {
-    link: `${baseRoute}/business`,
-    name: 'Register a buiness',
-    Icon: () => <BusinessIcon />,
-  },
-  {
-    link: `${baseRoute}/offerings`,
-    name: 'Find a business',
-    Icon: () => <LocalOfferIcon />,
-  },
-  {
-    link: `${baseRoute}/settings`,
+    link: `${baseRoute}/about`,
     name: 'About us',
-    Icon: () => <InfoIcon />,
+    Icon: Info,
   },
   {
-    link: `${baseRoute}/transactions`,
+    link: `${baseRoute}/faq`,
     name: 'FAQ',
-    Icon: () => <HelpIcon />,
+    Icon: FAQ,
+  },
+  {
+    link: `${baseRoute}/restaurants`,
+    name: 'Are you a restaurant?',
+    Icon: Restaurant,
+  },
+  {
+    link: `${baseRoute}/contact`,
+    name: 'Contact',
+    Icon: Email,
   },
 ];
 
