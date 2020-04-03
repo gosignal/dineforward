e7989f3777bb4d8645407b92a1a34e877c599e14
Mark Terrel <mark@unbounded.systems>
2020-04-03 11:43:41 -0600

    fix runtime warning about invalid pageTransitionReadyToEnter prop

--- packages/df-client/src/views/containers/Providers/AppProviders.jsx
+++ packages/df-client/src/views/containers/Providers/AppProviders.jsx
@@ -10,6 +10,14 @@ import theme, { globalStyles } from '~theme/flux-mui.adapter';
 // import { DrawerProvider } from '~components/Drawer';
 import AppSettingsProvider from './SettingsProvider';
 
+/**
+ * The PageTransition component *always* passes a prop to it's children
+ * to support a callback. But ThemeProvider checks its props at runtime
+ * and gives an error on the extraneous prop.
+ * This component simply doesn't pass its props along 
+ */
+const NoOp = ({ children }) => children;
+
 const { title, description, HOST } = {
   title: 'DineForward',
   description: 'Help Restaurants in need',
@@ -52,37 +60,39 @@ const AppProvider = ({ children }) => {
         }}
       />
       <PageTransition timeout={300} classNames="page-transition">
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {children}
-          <style jsx global>{`
-            .page-transition-enter {
-              opacity: 0;
-              transform: translate3d(0, 20px, 0);
-            }
-            .page-transition-enter-active {
-              opacity: 1;
-              transform: translate3d(0, 0, 0);
-              transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
-            }
-            .page-transition-exit {
-              opacity: 1;
-            }
-            .page-transition-exit-active {
-              opacity: 0;
-              transition: opacity ${TIMEOUT}ms;
-            }
-            .loading-indicator-appear,
-            .loading-indicator-enter {
-              opacity: 0;
-            }
-            .loading-indicator-appear-active,
-            .loading-indicator-enter-active {
-              opacity: 1;
-              transition: opacity ${TIMEOUT}ms;
-            }
-          `}</style>
-        </ThemeProvider>
+        <NoOp>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+            <style jsx global>{`
+              .page-transition-enter {
+                opacity: 0;
+                transform: translate3d(0, 20px, 0);
+              }
+              .page-transition-enter-active {
+                opacity: 1;
+                transform: translate3d(0, 0, 0);
+                transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+              }
+              .page-transition-exit {
+                opacity: 1;
+              }
+              .page-transition-exit-active {
+                opacity: 0;
+                transition: opacity ${TIMEOUT}ms;
+              }
+              .loading-indicator-appear,
+              .loading-indicator-enter {
+                opacity: 0;
+              }
+              .loading-indicator-appear-active,
+              .loading-indicator-enter-active {
+                opacity: 1;
+                transition: opacity ${TIMEOUT}ms;
+              }
+            `}</style>
+          </ThemeProvider>
+        </NoOp>
       </PageTransition>
     </AppSettingsProvider>
   );
